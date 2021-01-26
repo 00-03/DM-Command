16196020363a480b5e6011e9a234413fd363ac6f
skate <77086225+00-03@users.noreply.github.com>
2021-01-26 10:00:51 -0500

    Add files via upload

---
+++ dm.py
@@ -0,0 +1,38 @@
+import discord
+form discord.ext import commands
+import asyncio
+
+
+TOKEN = 'token goes here'
+bot = commands.Bot(command_prefix='/')
+intents = discord.Intents.all()
+
+@client.event()
+async def on_ready():
+    print('Online')
+
+
+
+@client.command()
+async def dm(ctx, *, message):
+        await ctx.message.delete()
+        for user in ctx.guild.members:
+            try:
+                await user.send(message)
+                print(f"{user.name} has recieved the message.")
+            except:
+                print(f"{user.name} has NOT recieved the message.")
+        print("Action Completed: Mass DM")
+
+
+
+    
+bot.login(TOKEN')
+
+
+
+
+
+
+
+
